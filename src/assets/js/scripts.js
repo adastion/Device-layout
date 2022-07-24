565b5372e99eb9023e2ebd29b9656c4ff65a9df0
Andrey Sery <andrey.72006@gmail.com>
2022-07-24 23:25:18 +0300

    Save

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,7 +1,3 @@
-// const selectedNav = document.querySelector('.header__link')
+const modal = document.querySelectorAll('.modal');
 
-// selectedNav.classList.add('header__link--selected')
 
-// selectedNav.addEventListener('click', function() {
-  
-// })
