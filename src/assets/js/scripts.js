cb8b80c562c0d29d144208a13c2e758abd695612
Andrey Sery <andrey.72006@gmail.com>
2022-07-26 01:09:51 +0300

    Save

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,3 +1,24 @@
-const modal = document.querySelectorAll('.modal');
+const modalBtn = document.querySelectorAll('[data-modal]');
+const modalClose = document.querySelectorAll('.modal__close');
 
+modalBtn.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    let $this = event.currentTarget;
+    let modalId = $this.getAttribute('data-modal');
+    let modal = document.getElementById(modalId);
 
+    modal.classList.add('modal--active');
+  });
+});
+
+modalClose.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    let currentModal = event.currentTarget.closest('.modal');
+
+    closeModal(currentModal);
+  });
+});
+
+const closeModal = (currentModal) => {
+  currentModal.classList.remove('modal--active');
+};
